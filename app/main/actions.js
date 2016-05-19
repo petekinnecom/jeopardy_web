82471642962bce2d97061fbabaa4672152d2f622
Pete Kinnecom <pete.kinnecom@gmail.com>
2016-05-18 20:39:27 -0600

    Rename gameData to board

--- app/main/actions.js
+++ app/main/actions.js
@@ -1,11 +1,11 @@
 export const START_GAME = "START_GAME"
 export const GAME_LOADED = "GAME_LOADED"
 
-export const gameLoaded = (gameData)=> {
+export const gameLoaded = (board)=> {
   return {
     type: GAME_LOADED,
-    gameData: gameData
-  }  
+    board: board
+  }
 }
 
 export const startGame = ()=> {
