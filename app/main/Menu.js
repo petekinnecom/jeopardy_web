987efa3beef2eed22ccfd3db62f08c187ebc5123
Martin Chase <outofculture@gmail.com>
2016-05-23 10:43:35 -0700

    log error, too

--- app/main/Menu.js
+++ app/main/Menu.js
@@ -29,7 +29,7 @@ const mapDispatchToProps = (dispatch) => {
         .then((board) => {
           dispatch(gameLoaded(board))
         })
-        .catch(()=>{console.log('noooooo')})
+        .catch((e)=>{console.log('noooooo', e)})
       })
     }
   }
